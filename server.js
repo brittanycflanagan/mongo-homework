bfeab6b1ed2e6f2f9fb9c4961149f79ff93774d3
Brittany Flanagan - Backdrop Express <brittanycturner@gmail.com>
2018-06-09 15:04:08 -0700

    removing articles

--- server.js
+++ server.js
@@ -341,30 +341,53 @@ app.get("/articles/:id", function(req, res) {
 app.post("/articles/:id", function(req, res) {
   // Create a new note and pass the req.body to the entry
   
-  db.Article.update(
-    {
-      _id: mongojs.ObjectId(req.params.id)
-    },
-    {
-      // Set "read" to true for the book we specified
-      $set: {
-        saved: true
-      }
-    },
-    // When that's done, run this function
-    function(error, edited) {
-      // show any errors
-      if (error) {
-        console.log(error);
-        res.send(error);
-      }
-      else {
-        // Otherwise, send the result of our update to the browser
-        console.log(edited);
-        res.send(edited);
-      }
-    }
-  );
+    // Create a new note and pass the req.body to the entry
+    db.Article.remove({ _id: req.params.id })
+      .then(function(dbArticle) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { justOne: true});
+      })
+      .then(function(dbNotes) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+
+  
+
+  //////////////////////////////
+  // db.Article.update(
+  //   {
+  //     _id: mongojs.ObjectId(req.params.id)
+  //   },
+  //   {
+  //     // Set "read" to true for the book we specified
+  //     $set: {
+  //       saved: true
+  //     }
+  //   },
+  //   // When that's done, run this function
+  //   function(error, edited) {
+  //     // show any errors
+  //     if (error) {
+  //       console.log(error);
+  //       res.send(error);
+  //     }
+  //     else {
+  //       // Otherwise, send the result of our update to the browser
+  //       console.log(edited);
+  //       res.send(edited);
+  //     }
+  //   }
+  // );
+
+  ///////////////////////////
+
     // .then(function(dbNote) {
     //   // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
     //   // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
